c75fb60a83981c8553adf823d511e0853fa41615
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-24 16:21:26 +0300

    Added material Ui

--- front-end/src/pages/home/index.tsx
+++ front-end/src/pages/home/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Button } from "@mui/material";
 const HomePage: React.FC = () => {
   return (
-    <div className=" bg-slate-400">
+    <div className="flex gap-2 items-center justify-center">
       <p>This home page </p>
+      <Button variant="outlined"> Home page</Button>
     </div>
   );
 };
