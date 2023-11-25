3785a7c14927e1d533077ea42e7a56356674834d
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-26 00:02:58 +0300

    error handling

--- api/index.js
+++ api/index.js
@@ -34,6 +34,17 @@ app.use("/api/users", usersRoute);
 app.use("/api/rooms", roomsRoute);
 app.use("/api/hostels", hostelsRoute);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
 app.listen(3000, () => {
   connect();
   console.log("Server is running on port 3000.");
