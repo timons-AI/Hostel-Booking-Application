1eba73df726846588da7cd482e5d0c85cdaf6ec4
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-25 23:51:32 +0300

    remaining get methods for hostels

--- api/routes/hostels.js
+++ api/routes/hostels.js
@@ -37,5 +37,39 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hostel = await Hostel.findById(req.params.id);
+    res.status(200).json(hostel);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 //GET ALL
+router.get("/", async (req, res) => {
+  const city = req.query.city;
+  const type = req.query.type;
+  const name = req.query.name;
+  const rating = req.query.rating;
+  const featured = req.query.featured;
+  try {
+    let hostels;
+    if (city) {
+      hostels = await Hostel.find({ city: city });
+    } else if (type) {
+      hostels = await Hostel.find({ type: type });
+    } else if (name) {
+      hostels = await Hostel.find({ name: name });
+    } else if (rating) {
+      hostels = await Hostel.find({ rating: { $gte: rating } });
+    } else if (featured) {
+      hostels = await Hostel.find({ featured: true });
+    } else {
+      hostels = await Hostel.find();
+    }
+    res.status(200).json(hostels);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 export default router;
