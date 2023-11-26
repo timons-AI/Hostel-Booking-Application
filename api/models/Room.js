09dd5a98cc28bd535a9700220500c354eae00394
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-26 14:00:15 +0300

    Rooms have been created and frontend was renamed
    renamed frontend folder to client,
    and created rooms routes in api

--- api/models/Room.js
+++ api/models/Room.js
@@ -25,3 +25,48 @@ const RoomSchema = new mongoose.Schema(
 );
 
 export default mongoose.models.Room || mongoose.model("Room", RoomSchema);
+
+// example of a model
+const room = {
+  title: "Single Room",
+  description: "A simple room with a bed and little space",
+  capacity: 1,
+  price: 100,
+  roomNumbers: [
+    {
+      number: 1,
+      unavailableDates: [
+        "2021-05-01T00:00:00.000Z",
+        "2021-05-02T00:00:00.000Z",
+      ],
+    },
+    {
+      number: 2,
+      unavailableDates: [
+        "2021-05-01T00:00:00.000Z",
+        "2021-05-02T00:00:00.000Z",
+      ],
+    },
+    {
+      number: 3,
+      unavailableDates: [
+        "2021-05-01T00:00:00.000Z",
+        "2021-05-02T00:00:00.000Z",
+      ],
+    },
+    {
+      number: 4,
+      unavailableDates: [
+        "2021-05-01T00:00:00.000Z",
+        "2021-05-02T00:00:00.000Z",
+      ],
+    },
+    {
+      number: 5,
+      unavailableDates: [
+        "2021-05-01T00:00:00.000Z",
+        "2021-05-02T00:00:00.000Z",
+      ],
+    },
+  ],
+};
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,29 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoomById,
+  getAllRooms,
+  getRoomById,
+  updateRoomById,
+} from "../controllers/room.js";
+
+import { verifyAdmin } from "../utils/verify-token.js";
 
 const router = express.Router();
+
+// create
+router.post("/:hostelId", verifyAdmin, createRoom);
+
+// update
+router.put("/:roomId", verifyAdmin, updateRoomById);
+
+// get all
+router.get("/", getAllRooms);
+
+// get one
+router.get("/:Id", getRoomById);
+
+// delete one
+router.delete("/:roomId/:hotelId", verifyAdmin, deleteRoomById);
 
 export default router;
