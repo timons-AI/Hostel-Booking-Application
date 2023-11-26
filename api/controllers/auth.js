46d46a2f48ffdd3abf8e8b0cb66093cb01de6d81
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-26 03:21:55 +0300

    register and login
    change to use jwt tokens for security and checking if the user is admin

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bycrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   try {
@@ -13,6 +14,40 @@ export const register = async (req, res, next) => {
     });
     const user = await newUser.save();
     res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username }).select(
+      "+password"
+    );
+    !user && res.status(400).json("Wrong credentials");
+    console.log(user);
+
+    const validated = await bycrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+    !validated && res.status(400).json("Wrong credentials");
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res
+      .cookie("token", accessToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      })
+      .status(200)
+      .json(others);
   } catch (error) {
     next(error);
   }
--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import roomsRoute from "./routes/rooms.js";
 import hostelsRoute from "./routes/hostels.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -28,7 +29,9 @@ mongoose.connection.on("connected", () => {
 
 // middlewares
 
+app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/rooms", roomsRoute);
