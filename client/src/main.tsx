8deb796405a226a5900e9045c5753d416937ccd5
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-28 08:06:18 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { AuthContextProvider } from "./context/AuthContext.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <App />
   </React.StrictMode>
 );
