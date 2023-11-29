180da57dc37cdd4ff980d831432a465c3ac175be
mrbillionn <kyaligonzawarren44@gmail.con>
2023-11-29 12:03:41 +0300

    Update hero-section.tsx

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -7,18 +7,7 @@ export default function HeroSection() {
         <div className="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-48 lg:pb-56 xl:col-span-6">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <img className="h-11" src="/vite.svg" alt="Your Company" />
-            <div className="hidden sm:mt-32 sm:flex lg:mt-16">
-              <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Made for ease by those in need.{" "}
-                <a
-                  href="#"
-                  className="whitespace-nowrap font-semibold text-indigo-600"
-                >
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
-            </div>
+           
             <h1 className="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
               No more Looking Up and Down.
               <br />
