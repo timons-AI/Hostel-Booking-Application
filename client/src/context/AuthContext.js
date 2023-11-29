661ddd493104ed71e5f1c687d80a2498b39c73ee
mrbillionn <kyaligonzawarren44@gmail.con>
2023-11-29 12:43:04 +0300

    auth context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,13 +1,24 @@
+/*
+  This file defines a React context and a reducer for managing authentication state.
+  It provides an initial state, actions for login success/failure, logout, and a context provider.
+  The context provider uses the reducer and manages the state using the useReducer hook.
+  It also persists user information to local storage.
+*/
+
+// Import necessary dependencies from React
 import { createContext, useEffect, useReducer } from "react";
 
+// Define the initial state for the authentication context
 const INITIAL_STATE = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   loading: false,
   error: null,
 };
 
+// Create the authentication context using createContext
 export const AuthContext = createContext(INITIAL_STATE);
 
+// Define the authentication reducer function
 const AuthReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN_START":
@@ -39,13 +50,17 @@ const AuthReducer = (state, action) => {
   }
 };
 
+// Create the context provider component for the authentication context
 export const AuthContextProvider = ({ children }) => {
+  // Use the useReducer hook to manage state with the defined reducer
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
+  // Effect hook to persist user information to local storage
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.user));
   }, [state.user]);
 
+  // Return the context provider with the provided value and children
   return (
     <AuthContext.Provider
       value={{
