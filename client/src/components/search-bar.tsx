1ef9b06a5516d36b13201eaa72f71be769f36111
mrbillionn <kyaligonzawarren44@gmail.con>
2023-11-29 12:17:29 +0300

    search bar

--- client/src/components/search-bar.tsx
+++ client/src/components/search-bar.tsx
@@ -1,3 +1,4 @@
+// Import necessary components and libraries
 import { DateRange } from "react-date-range";
 import { Box, Modal } from "@mui/material";
 import { useState } from "react";
@@ -5,7 +6,9 @@ import { format } from "date-fns";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
+// Define the functional component SearchBar
 const SearchBar = () => {
+  // State to manage the selected date range
   const [date, setDate] = useState([
     {
       startDate: new Date(),
@@ -14,6 +17,7 @@ const SearchBar = () => {
     },
   ]);
 
+  // Style object for the modal
   const style = {
     position: "absolute" as "absolute",
     top: "50%",
@@ -22,22 +26,26 @@ const SearchBar = () => {
     width: 400,
     bgcolor: "background.paper",
     borderRadius: 10, // You can adjust the value to control the roundness
-
     boxShadow: 24,
     p: 4,
   };
 
+  // State and functions for managing modal open/close
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  // Render the SearchBar component
   return (
-    <div className=" bg-slate-100 w-full p-5 rounded-lg">
+    <div className="bg-slate-100 w-full p-5 rounded-lg">
       <form>
         <div className="flex">
+          {/* Label for search location */}
           <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
             Search location
           </label>
+          
+          {/* Button to open date range modal */}
           <button
             onClick={handleOpen}
             id="dropdown-button"
@@ -45,7 +53,8 @@ const SearchBar = () => {
             className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
             type="button"
           >
-            <span className=" text-slate-500  flex items-center gap-2   text-sm">
+            <span className="text-slate-500 flex items-center gap-2 text-sm">
+              {/* Display selected date range or default text */}
               {date[0].startDate === date[0].endDate
                 ? "Check in"
                 : `${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
@@ -70,6 +79,7 @@ const SearchBar = () => {
             </svg>
           </button>
 
+          {/* Modal for selecting date range */}
           <Modal
             open={open}
             onClose={handleClose}
@@ -86,14 +96,17 @@ const SearchBar = () => {
             </Box>
           </Modal>
 
+          {/* Input for searching by location */}
           <div className="relative w-full">
             <input
               type="search"
               id="search-dropdown"
-              className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+              className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
               placeholder="Search by city, hotel, or neighborhood"
               required
             />
+            
+            {/* Search button */}
             <button
               type="submit"
               className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -122,4 +135,5 @@ const SearchBar = () => {
   );
 };
 
+// Export the SearchBar component
 export default SearchBar;
